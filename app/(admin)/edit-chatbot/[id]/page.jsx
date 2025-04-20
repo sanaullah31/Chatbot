bca855058a491d9b080ad83a1f4b4473848fd2dc
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-04-20 10:21:04 +0530

    live url

--- app/(admin)/edit-chatbot/[id]/page.jsx
+++ app/(admin)/edit-chatbot/[id]/page.jsx
@@ -21,8 +21,8 @@ const Page = () => {
     const [chars, setChars] = useState([])
 
     const router = useRouter()
-    const base_url = 'http://localhost:3000'
-    // const base_url = 'https://chat-bot-pbc.vercel.app'
+    // const base_url = 'http://localhost:3000'
+    const base_url = 'https://chat-bot-pbc.vercel.app'
 
     useEffect(() => {
         setUrl(`${base_url}/chatbot/${params.id}`)
